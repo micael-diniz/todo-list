935d1fd3a7edb6f0ca621ff77bae219029f811b6
Micael Diniz <micaeldiniz6@gmail.com>
2023-10-15 01:16:38 -0300

    fix(empty list): centralize texts

--- src/components/molecules/ToDoListEmptyList/ToDoListEmptyListMessage.tsx
+++ src/components/molecules/ToDoListEmptyList/ToDoListEmptyListMessage.tsx
@@ -3,7 +3,7 @@ import { Text } from "../../atoms/Text";
 export function ToDoListEmptyListMessage() {
   return (
     <div>
-      <Text className="font-bold text-gray-300">
+      <Text className="font-bold text-gray-300 text-center">
         You don't have tasks registered yet
       </Text>
       <Text className="font-normal text-gray-300">Create tasks and organize your to-do items</Text>
